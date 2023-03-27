25a91bcc9df746f5ea2faad32a41ca343fba548d
unknown <sakibovi123@gmail.com>
2023-03-27 14:33:18 +0600

    chat gpt responsive

--- pages/home/components/home-solutions.php
+++ pages/home/components/home-solutions.php
@@ -10,7 +10,12 @@
   <!-- section header ends here -->
 
   <!-- section description starts here -->
-  <div class="solution-tab-text-left flex items justify-between items-center mb-20">
+
+
+
+
+    
+  <div class="solution-tab-text-left flex justify-between items-center mb-20">
 
     <div class="text-part w-5/12 ">
       <div class="head mb-3.5">
@@ -89,6 +94,79 @@
     </div>
 
   </div>
+
+  <!-- Chat gpt -->
+  <div class="solution-tab-text-left flex flex-col md:flex-row items-center md:items-stretch justify-between mb-20">
+  <div class="text-part w-full md:w-5/12 md:pr-10">
+    <div class="head mb-3.5">
+      <span class="iconify inline-block text-5xl mr-3.5" data-icon="eos-icons:machine-learning-outlined"></span>
+      <span class="text-xl font-bold">e-Learning</span>
+    </div>
+
+    <div class="title capitalize mb-3.5 text-2xl font-semibold">
+      AI-Powered cloud-based elearning system
+    </div>
+
+    <div class="text-base text-gray-600 leading-7 mb-5">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore unde ex voluptatibus est, veritatis pariatur
+      ea eaque, tempora mollitia, nulla ullam animi vero ut repudiandae ipsam cupiditate obcaecati quibusdam cumque!
+    </div>
+
+    <div class="text-base text-gray-600 leading-7 mb-5">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias sed fugit aliquid, aut aperiam, voluptatum
+      accusantium voluptates tempore expedita mollitia delectus, possimus laboriosam ipsam qui. Labore perspiciatis
+      quae iusto? Perspiciatis!
+    </div>
+
+    <div class="text-gray-600 leading-6 mb-3.5">
+      <ul class="our-solution-points">
+        <li>
+          <span class="iconify inline-block text-xl align-middle" data-icon="material-symbols:arrow-right-alt"></span>
+          <span class="capitalize">Student & teachers management</span>
+        </li>
+
+        <li>
+          <span class="iconify inline-block text-xl align-middle" data-icon="material-symbols:arrow-right-alt"></span>
+          <span class="capitalize">class & course manager</span>
+        </li>
+
+        <li>
+          <span class="iconify inline-block text-xl align-middle" data-icon="material-symbols:arrow-right-alt"></span>
+          <span class="capitalize">online admission management</span>
+        </li>
+
+        <li>
+          <span class="iconify inline-block text-xl align-middle" data-icon="material-symbols:arrow-right-alt"></span>
+          <span class="capitalize">admin, student, teacher dashboard</span>
+        </li>
+
+        <li>
+          <span class="iconify inline-block text-xl align-middle" data-icon="material-symbols:arrow-right-alt"></span>
+          <span class="capitalize">seamless interaction between teachers and students</span>
+        </li>
+      </ul>
+    </div>
+
+    <div class="text-gray-600 hover:opacity-80">
+      <a href="#">
+        learn more about e-Learning <span class="iconify inline-block text-xl" data-icon="ph:arrow-circle-right-fill"></span>
+      </a>
+    </div>
+  </div>
+
+  <div class="image-part w-full md:w-5/12">
+    <img class="rounded-2xl mx-auto md:ml-0 md:mr-auto" src="assets/logos/e-learning.jpg" alt="e-learning-image">
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
 
   <div class="solution-tab-text-right flex items justify-between items-center mb-20">
 
