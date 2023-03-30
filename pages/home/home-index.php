d9cab42d14919c5d67e6b7a6fe2536fe79142234
Faiz <faizahmed23014@example.com>
2023-03-31 00:31:24 +0600

    about company section done with responsive

--- pages/home/home-index.php
+++ pages/home/home-index.php
@@ -3,5 +3,6 @@
 <?php include 'pages/home/components/home-banner.php';?>
 <?php include 'pages/home/components/home-solutions.php';?>
 <?php include 'pages/home/components/home-service.php';?>
+<?php include 'pages/home/components/about-company.php';?>
 
 <?php 'footer.php' ?>
