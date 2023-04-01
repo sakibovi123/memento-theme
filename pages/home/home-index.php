bda40e09b0a8ca44727ee444e15d26e670ac2278
unknown <sakibovi123@gmail.com>
2023-04-01 15:26:50 +0600

    update 1

--- pages/home/home-index.php
+++ pages/home/home-index.php
@@ -1,8 +1,8 @@
 <?php include 'header.php'; ?>
 
-<?php include 'pages/home/components/home-banner.php';?>
-<?php include 'pages/home/components/home-solution-2.php';?>
-<?php include 'pages/home/components/home-service.php';?>
+<?php include 'pages/home/components/home-banner.php'; ?>
+<?php include 'pages/home/components/home-solution-2.php'; ?>
+<?php include 'pages/home/components/home-service.php'; ?>
 <?php include 'pages/home/components/home-testimonials.php'; ?>
 
 <?php 'footer.php' ?>
